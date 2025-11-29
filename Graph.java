a2abaeba1f2b56addc28c8ad7cfa51fc88316a55
UK-04 <usmankpc72@gmail.com>
2025-11-29 08:16:17 -0600

    I added my name

--- Graph.java
+++ Graph.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Usman Khan / COMP 272-002
  *
  *   Note, additional comments provided throughout this source code
  *   is for educational purposes
